d01f58ba00949620cb06489620a1c6a8ec750cc9
Jonnerop <142784165+Jonnerop@users.noreply.github.com>
2024-02-12 17:34:49 +0200

    Create tehtävä 1.py

---
+++ tehtävä 1.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+# a)
+a1 = np.array([[-1, 2], [3, 1]])
+a2 = np.array([[0, 1, 3], [2, -3, 5]])
+result_a = np.dot(a1, a2)
+
+# b)
+b1 = np.array([[1, 3, 5], [0, -2, 1], [2, -1, 4]])
+b2 = np.array([[1], [-3], [-1]])
+result_b = np.dot(b1, b2)
+
+# c)
+c1 = np.array([[2, 0, 1], [1, -3, 4], [0, 1, 5]])
+c2 = np.array([[3], [-5], [7]])
+result_c = np.dot(c1, c2)
+
+# d)
+d1 = np.array([[1, -4, 2], [3, 0, -2], [2, 1, 0]])
+d2 = np.array([[5, 1, -1], [-2, 1, 3], [0, 3, 4]])
+result_d = np.dot(d1, d2)
+
+print("a) vastaus:")
+print(result_a)
+print("b) vastaus:")
+print(result_b)
+print("c) vastaus:")
+print(result_c)
+print("d) vastaus:")
+print(result_d)
